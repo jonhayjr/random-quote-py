fbb33c00d0d7b8c1ce78fd74863ffe68a0d05f60
jonhayjr <jonhayjr@gmail.com>
2025-05-15 21:47:29 -0600

    added quotes list

--- RandomQuoteGenerator.py
+++ RandomQuoteGenerator.py
@@ -1,3 +1,8 @@
 #import random module
 import random
 
+# quotes list
+quotes = [["I believe we are here on the planet Earth to live, grow up and do what we can to make this world a better place for all people to enjoy freedom.", "Rosa Parks" 
+], ["To love oneself is the beginning of a lifelong romance.", "Oscar Wilde"], ["No one can make you feel inferior without your consent.", "Eleanor Roosevelt"], ["To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.", "Ralph Waldo Emerson"]]
+
+print(quotes)
